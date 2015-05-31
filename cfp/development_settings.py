386ea4ed82402e770cbf3f4eb4377c9fd4dd0dc2
Ben Fishman <benjafishman@gmail.com>
2015-05-30 23:26:58 -0400

    - setting up heroku

--- cfp/development_settings.py
+++ cfp/development_settings.py
@@ -6,7 +6,6 @@ DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
-
 DATABASES = {
  'default': {
  'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -16,3 +15,23 @@ DATABASES = {
  'HOST': 'ec2-174-129-26-115.compute-1.amazonaws.com'
  }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+ os.path.join(BASE_DIR, '../static'),
+)
