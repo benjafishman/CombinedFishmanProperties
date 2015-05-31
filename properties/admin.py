ce68215bfdf3a43cf71e34e79b51e74c772c54cb
Ben Fishman <benjafishman@gmail.com>
2015-05-31 12:44:50 -0400

    - static files bootstrap/responsiveslide - home/ base template

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Property
 
 # Register your models here.
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    fields = ['name', 'address', 'description']
+
+admin.site.register(Property,PropertyAdmin)
