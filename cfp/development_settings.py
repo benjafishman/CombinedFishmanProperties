3ec9ba17752e6ae1e8a9ec627d166a91dc73d6e4
Ben Fishman <benjafishman@gmail.com>
2015-05-30 22:37:02 -0400

    initial commit of cfp

---
+++ cfp/development_settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from .settings import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ 'NAME': 'darmoactn9ipc6', 
+ 'USER': 'jeoroaceocxqvb',
+ 'PASSWORD': 'EVNT7ZRX9FK1YUU0qZ67JdTAiV',
+ 'HOST': 'ec2-174-129-26-115.compute-1.amazonaws.com'
+ }
+}
