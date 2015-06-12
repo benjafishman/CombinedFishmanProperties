3cecbb609d5307eea5b38f0dae74f85e1ac3382d
Ben Fishman <benjafishman@gmail.com>
2015-06-12 18:52:19 -0400

    - updating settings

--- cfp/development_settings.py
+++ cfp/development_settings.py
@@ -7,13 +7,6 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.postgresql_psycopg2',
- 'NAME': 'darmoactn9ipc6', 
- 'USER': 'jeoroaceocxqvb',
- 'PASSWORD': 'EVNT7ZRX9FK1YUU0qZ67JdTAiV',
- 'HOST': 'ec2-174-129-26-115.compute-1.amazonaws.com'
- }
 }
 
 # Parse database configuration from $DATABASE_URL
