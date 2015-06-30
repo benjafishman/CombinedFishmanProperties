a2911df99296e07badee0704a6838efcdba7491d
Ben Fishman <benjafishman@gmail.com>
2015-06-29 21:00:43 -0400

    - adding view specific titles - removed random hello world text

--- properties/views.py
+++ properties/views.py
@@ -6,12 +6,13 @@ from .models import Property
 
 def index(request):
     properties = Property.objects.all()
-    print(properties)
-    context = {'properties': properties }
+    context = {'properties': properties, 'title':'cfp' }
     return render(request, 'properties/home.html', context)
 
 def contact(request):
-    return render(request,'properties/contact.html')
+    context = {'title':'Contact' }
+    return render(request,'properties/contact.html', context)
 
 def about(request):
-    return render(request,'properties/about.html')
+    context = {'title':'About Us' }
+    return render(request,'properties/about.html', context)
