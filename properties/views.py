895db9529f502b838e3d165d349b5397c343d0c4
Ben Fishman <benjafishman@gmail.com>
2015-08-05 23:10:46 -0400

    - adding property listing drop down - adding property object to each view - a lot of template changes

--- properties/views.py
+++ properties/views.py
@@ -5,19 +5,24 @@ from .models import Property
 # Create your views here.
 
 def index(request):
-    properties = Property.objects.all()
+    properties = Property.objects.all().order_by('pk')
     context = {'properties': properties, 'title':'cfp' }
     return render(request, 'properties/home.html', context)
 
 def contact(request):
-    context = {'title':'Contact' }
+    properties = Property.objects.all().order_by('pk')
+    context = {'properties': properties, 'title':'Contact' }
     return render(request,'properties/contact.html', context)
 
 def about(request):
-    context = {'title':'About Us' }
+    properties = Property.objects.all().order_by('pk')
+    context = {'properties': properties, 'title':'About Us' }
     return render(request,'properties/about.html', context)
 
 def propertyDetail(request, property_id):
+    # Exclude the property being viewed from the drop down list of properties
+    properties = Property.objects.all().exclude(pk=property_id).order_by('pk')
     property = get_object_or_404(Property, pk=property_id)
     loop_times = range(0,property.number_of_layouts)
-    return render(request, 'properties/detail.html', {'property': property, 'loop_times': loop_times})
+    context = {'properties': properties, 'property': property, 'loop_times': loop_times}
+    return render(request, 'properties/detail.html', context)
